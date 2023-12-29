636874c507d7d933de883396782c5e5dab3a6010
Jon Surrell <sirreal@users.noreply.github.com>
2023-12-29 20:08:09 +0100

    working build

--- main.asset.php
+++
@@ -1,9 +0,0 @@
-<?php return array(
-	'dependencies' => array(
-		// array('id' => '@wordpress/interactivity', 'type' => 'dynamic')
-		array('id' => '@wordpress/interactivity', 'type' => 'static'),
-		// '@wordpress/interactivity',
-	),
-	'version' => '6caa14fd282d5d74ff07',
-	'type' => 'module'
-);
--- plugin.php
+++ plugin.php
@@ -6,39 +6,7 @@
 add_action(
 	'init', 
 	function () {
-
-		if ( defined( 'IS_GUTENBERG_PLUGIN' ) && IS_GUTENBERG_PLUGIN ) {
-
-			$module_path       = plugins_url('/dist/main.js', __FILE__);
-			$module_asset_path = __DIR__ . '/dist/main.asset.php';
-			$module_asset      = require( $module_asset_path );
-
-			gutenberg_register_module(
-				'jon/the-block',
-				$module_path,
-				$module_asset['dependencies'],
-				$module_asset['version'],
-			);
-
-			register_block_type(
-				'jon/the-block',
-				array(
-					'title' => 'jonnn',
-					'render_callback' => function () {
-						gutenberg_enqueue_module( 'jon/the-block' );
-						return <<<HTML
-	<div data-wp-interactive='{"namespace":"jon/the-block"}'>
-		<input readonly type='number' data-wp-bind--value='state.counter' />
-		<button data-wp-on--click='actions.inc'>+</button>
-		<button data-wp-on--click='actions.dec'>-</button>
-	</div>
-	HTML;
-					},
-				)
-			);
-
-			
-		}
+		register_block_type_from_metadata(__DIR__ . '/build/block.json');
 	}
 );
 
