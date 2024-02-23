98034ce1fa9756dc0be4a983502841a79cb7a13b
Jon Surrell <sirreal@users.noreply.github.com>
2024-02-23 16:36:36 +0100

    simple

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import * as React from "react";
-import metadata from "./block.json";
-import { registerBlockType } from "@wordpress/blocks";
+import * as React from 'react';
+import metadata from './block.json';
+import { registerBlockType } from '@wordpress/blocks';
 
 registerBlockType(metadata, {
-  edit() {
-    return <div>counter</div>;
-  },
-  save() {
-    return <div>counter</div>;
-  },
+	edit() {
+		return <div>counter</div>;
+	},
+	save() {
+		return <div>counter</div>;
+	},
 });
